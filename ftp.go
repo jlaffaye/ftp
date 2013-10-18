169efda0b5b7bc08de464393bde23da76443dfe8
Daniel Sluis <daniel@lulus.com>
2013-10-18 09:30:28 -0700

    Changing expected status response for Logout call

--- ftp.go
+++ ftp.go
@@ -437,7 +437,7 @@ func (c *ServerConn) NoOp() error {
 
 // Logout issues a REIN FTP command to logout the current user.
 func (c *ServerConn) Logout() error {
-	_, _, err := c.cmd(StatusLoggedIn, "REIN")
+	_, _, err := c.cmd(StatusReady, "REIN")
 	return err
 }
 
