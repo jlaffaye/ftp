5d41901190670f202a4aafb39c81d72e2e42bd8d
Julien Laffaye <julien.laffaye@shopify.com>
2021-03-06 19:44:19 -0500

    Fix issues reported by goimports

--- scanner_test.go
+++ scanner_test.go
@@ -1,7 +1,10 @@
 package ftp
 
-import "testing"
-import "github.com/stretchr/testify/assert"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestScanner(t *testing.T) {
 	assert := assert.New(t)
