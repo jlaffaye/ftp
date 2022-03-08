48e53fcac1819eb6d355b0e7dbde127d0bf30339
Julien Laffaye <jlaffaye@freebsd.org>
2022-03-08 18:57:09 -0500

    Add tests for EntryType

--- constants_test.go
+++ constants_test.go
@@ -10,3 +10,9 @@ func TestStatusText(t *testing.T) {
 	assert.Equal(t, "Unknown status code: 0", StatusText(0))
 	assert.Equal(t, "Invalid username or password.", StatusText(StatusInvalidCredentials))
 }
+
+func TestEntryTypeString(t *testing.T) {
+	assert.Equal(t, "file", EntryTypeFile.String())
+	assert.Equal(t, "folder", EntryTypeFolder.String())
+	assert.Equal(t, "link", EntryTypeLink.String())
+}
